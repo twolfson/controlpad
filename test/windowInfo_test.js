bf1d6028e2bf39f0222082c8368abf59a5f8d52b
Todd Wolfson <todd@twolfson.com>
2013-07-03 01:49:15 -0700

    Successfully testing more properties of windowInfo

--- test/windowInfo_test.js
+++ test/windowInfo_test.js
@@ -1,22 +1,43 @@
 // Load in dependencies
 var windowInfo = require('../lib/windowInfo'),
-    exec = require('../lib/utils').exec,
+    execSync = require('../lib/utils').exec,
+    exec = require('child_process').exec,
     expect = require('chai').expect;
 
 // Set up test global
 var WINDOW_ID = '0x01600093',
-    COMMON_NAME = exec('wmctrl -l | grep ' + WINDOW_ID + ' | cut --delimiter " " --fields 5-');
+    COMMON_NAME = execSync('wmctrl -l | grep ' + WINDOW_ID + ' | cut --delimiter " " --fields 5-');
 describe('windowInfo', function () {
   describe('getting info about a window at 0,0', function () {
+    // Before anything, resize the window
+    before(function (done) {
+      // Sorry, tests are designed for 1920 x 1080 (x2) setup
+      // left, top, width, height
+      exec('wmctrl -r "' + COMMON_NAME + '" -e 0,0,0,1000,900', done);
+    });
 
+    // Grab the window dimentsions
+    before(function () {
+      this.win = windowInfo(WINDOW_ID);
+    });
+
+    it('returns proper window size', function () {
+      expect(this.win).to.deep.equal({
+        // DEV: wmctrl resizes left +3 and top +24 so we calculate proper height and such including border
+        left: 0,
+        top: 0,
+        width: 1003,
+        height: 924
+      });
+    });
   });
 
   describe('getting info about a window at an arbitrary location', function () {
     // Before anything, resize the window
-    before(function () {
+    before(function (done) {
       // Sorry, tests are designed for 1920 x 1080 (x2) setup
-      // Left, top, width, height
-      exec('wmctrl -r "' + COMMON_NAME + '" -e 0,2800,30,1000,900');
+      // left, top, width, height
+      exec('wmctrl -r "' + COMMON_NAME + '" -e 0,2800,30,1000,900', done);
     });
 
     // Grab the window dimentsions
