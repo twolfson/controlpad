0f4f44e67ef6daedccc6686ec88a056c1dd2b8ff
Todd Wolfson <todd@twolfson.com>
2013-07-03 01:38:59 -0700

    Testing out window resizing and being inaccurate as expected

---
+++ test/resizeWindowTo_test.js
@@ -0,0 +1,32 @@
+// Load in dependencies
+var resizeWindowTo = require('../lib/resizeWindowTo'),
+    windowInfo = require('../lib/windowInfo'),
+    exec = require('../lib/utils').exec,
+    expect = require('chai').expect;
+
+// Set up test global
+var WINDOW_ID = '0x01600093',
+    COMMON_NAME = exec('wmctrl -l | grep ' + WINDOW_ID + ' | cut --delimiter " " --fields 5-');
+describe.only('resizeWindowTo', function () {
+  describe('resizing a window', function () {
+    before(function () {
+      resizeWindowTo(COMMON_NAME, {
+        top: 20,
+        left: 2900,
+        height: 800,
+        width: 400
+      });
+    });
+
+    it('is successful in its resize', function () {
+      var info = windowInfo(WINDOW_ID);
+      console.log(info);
+      expect(info).to.deep.equal({
+        top: 20,
+        left: 2900,
+        width: 800,
+        height: 400
+      });
+    });
+  });
+});
