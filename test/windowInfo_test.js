00d62d024b3eea2633fe7474c9405dd99439660e
Todd Wolfson <todd@twolfson.com>
2013-07-03 00:05:36 -0700

    Got resize working but dimensions seem off by border

--- test/windowInfo_test.js
+++ test/windowInfo_test.js
@@ -4,13 +4,15 @@ var windowInfo = require('../lib/windowInfo'),
     expect = require('chai').expect;
 
 // Set up test global
-var WINDOW_ID = '0x1600093';
+var WINDOW_ID = '0x01600093',
+    COMMON_NAME = exec('wmctrl -l | grep ' + WINDOW_ID + ' | cut --delimiter " " --fields 5');
+console.log(COMMON_NAME);
 describe('windowInfo', function () {
   // Before anything, resize the window
   before(function () {
     // Sorry, tests are designed for 1920 x 1080 (x2) setup
     // Left, top, width, height
-    exec('wmctrl -r ' + WINDOW_ID + ' -e 0,2800,30,1000,1100');
+    exec('wmctrl -r ' + COMMON_NAME + ' -e 0,2800,30,1000,900');
   });
 
   // Grab the window dimentsions
@@ -24,7 +26,7 @@ describe('windowInfo', function () {
       left: 2800,
       top: 30,
       width: 1000,
-      height: 1100
+      height: 900
     });
   });
 });
