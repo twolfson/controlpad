aab2211272a455e0e19d99ba9140d46918e83462
Todd Wolfson <todd@twolfson.com>
2014-10-08 16:45:31 +0900

    Adding mock logic

--- test/getDisplays_test.js
+++ test/getDisplays_test.js
@@ -1,23 +1,35 @@
 var getDisplays = require('../lib/getDisplays'),
     expect = require('chai').expect;
 
-describe('getDisplays', function () {
+describe.only('getDisplays', function () {
   before(function () {
     this.displays = getDisplays();
   });
 
-  it('returns proper monitor sizes', function () {
-    // Sorry, tests are designed for 1920 x 1080 (x2) setup
+  it('returns accurate monitor size', function () {
     expect(this.displays).to.deep.equal([{
       top: 0,
       left: 0,
       width: 1920,
       height: 1080
-    }, {
-      top: 0,
-      left: 1920,
-      width: 1920,
-      height: 1080
     }]);
+  });
+
+  describe.skip('on 2 monitors', function () {
+    // TODO: Add mock
+
+    it('returns accurate monitor sizes', function () {
+      expect(this.displays).to.deep.equal([{
+        top: 0,
+        left: 0,
+        width: 1920,
+        height: 1080
+      }, {
+        top: 0,
+        left: 1920,
+        width: 1920,
+        height: 1080
+      }]);
+    });
   });
 });
