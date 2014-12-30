691d512cc6fa27cfc0a783054f34fd35d9d8d72d
Todd Wolfson <todd@twolfson.com>
2014-12-29 22:17:16 -0600

    Not sure if I can trust the tests or code anymore...

--- test/resizeWindowTo_test.js
+++ test/resizeWindowTo_test.js
@@ -5,13 +5,14 @@ var resizeWindowTo = require('../lib/resizeWindowTo'),
     expect = require('chai').expect;
 
 // Set up test global
-var WINDOW_ID = '0x01600093';
-describe.skip('resizeWindowTo', function () {
+// DEV: Find an id via `wmctrl -l` on any window
+var WINDOW_ID = '0x0360008a';
+describe('resizeWindowTo', function () {
   before(function setupName (done) {
     // TODO: Use shell-quote
     var that = this;
     exec('wmctrl -l | grep ' + WINDOW_ID + ' | cut --delimiter " " --fields 5-', function handleName (err, name) {
-      that.commonName = name;
+      that.commonName = name.trim();
       done(err);
     });
   });
@@ -20,22 +21,27 @@ describe.skip('resizeWindowTo', function () {
   });
 
   describe('resizing a window', function () {
-    before(function () {
-      resizeWindowTo(COMMON_NAME, {
+    before(function (done) {
+      resizeWindowTo(this.commonName, {
         top: 20,
         left: 2900,
         height: 800,
         width: 400
-      });
+      }, done);
     });
 
-    it('is successful in its resize', function () {
-      var info = windowInfo(WINDOW_ID);
-      expect(info).to.deep.equal({
-        top: 20,
-        left: 2900,
-        height: 800,
-        width: 400
+    it('is successful in its resize', function (done) {
+      info = windowInfo(WINDOW_ID, function handleWindowInfo (err, info) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(info).to.deep.equal({
+          top: 20,
+          left: 2900,
+          height: 800,
+          width: 400
+        });
       });
     });
   });
